8aa302e6c1f6a46aaf6d2f9507528bc5cfc7607d
Drazbaz <62464834+Drazbaz@users.noreply.github.com>
2023-07-08 12:27:01 +0100

    routing updates

--- src/App.svelte
+++ src/App.svelte
@@ -2,7 +2,43 @@
   import { onMount } from "svelte";
 
   import HomePage from "./lib/home/HomePage.svelte";
-  import ProjectsPage from "./lib/projects/ProjectsPage.svelte";
+  import ExperiencePage from "./lib/experience/ExperiencePage.svelte";
+  import ProjectPage from "./lib/projects/ProjectPage.svelte";
 
-  import { routes } from "./routes";
+  let currentPath = "";
+
+  const navigateTo = (path) => {
+    currentPath = path;
+    window.history.pushState({}, "", path);
+  };
+
+  onMount(() => {
+    window.addEventListener("popstate", () => {
+      currentPath = window.location.pathname;
+    });
+
+    currentPath = window.location.pathname;
+  });
 </script>
+
+<nav>
+  <a href="/" on:click|preventDefault={() => navigateTo("/")}> Home </a>
+  <a
+    href="/experience"
+    on:click|preventDefault={() => navigateTo("/experience")}
+  >
+    Experience
+  </a>
+  <a href="/project" on:click|preventDefault={() => navigateTo("/project")}>
+    Project
+  </a>
+</nav>
+{#if currentPath === "/"}
+  <HomePage />
+{:else if currentPath === "/experience"}
+  <ExperiencePage />
+{:else if currentPath === "/project"}
+  <ProjectPage />
+{:else}
+  <p>404 - Page Not Found</p>
+{/if}
--- src/routes.js
+++
@@ -1,7 +0,0 @@
-import HomePage from "./lib/HomePage.svelte";
-import ProjectsPage from "./lib/ProjectsPage.svelte";
-
-export const routes = [
-    { path: '/', component: HomePage },
-    { path: '/projects', component: ProjectsPage }
-];
